14c64f347510ef1b525ed0d02d9d5ef74bd1be30
Jatin Kushwah <singhjatin354@gmail.com>
2023-02-13 19:13:18 +0530

    base url updated

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.use(cookieParser());
 
 let origin = "http://localhost:3000";
 if (process.env.NODE_ENV === "production") {
-    origin = process.env.CORS_ORIGIN;
+    origin = "https://client-app-e2xl.onrender.com/";
 }
 
 app.use(
