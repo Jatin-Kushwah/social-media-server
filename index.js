2e4044fd3cbcc3376f9ddaaec87291bb16db4ae0
Jatin Kushwah <singhjatin354@gmail.com>
2023-02-13 19:31:00 +0530

    cors origin updated.

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.use(cookieParser());
 
 let origin = "http://localhost:3000";
 if (process.env.NODE_ENV === "production") {
-    origin = "https://client-app-e2xl.onrender.com/";
+    origin = "https://social-media-client-mocha.vercel.app/";
 }
 
 app.use(
